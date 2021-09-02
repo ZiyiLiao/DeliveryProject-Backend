6d14b512db1006b20108e7c5dd56287fc7aafcc6
ZiyiLiao <eziyi1996@icloud.com>
2021-09-01 19:14:42 -0700

    OWLY-lucy-tableCreation: create Station, Tracking, Vehicle table

---
+++ src/main/java/com/owly/delivery/entity/Tracking.java
@@ -0,0 +1,68 @@
+package com.owly.delivery.entity;
+
+
+import com.owly.delivery.enums.ShipmentStatus;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Tracking {
+    @Id
+    private int trackingId;
+    private ShipmentStatus shipmentStatus;
+
+    @ManyToOne
+    private Orders order;
+
+    @ManyToOne
+    private Station station;
+
+    @ManyToOne
+    private Vehicle vehicle;
+
+    public Orders getOrder() {
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public ShipmentStatus getShipmentStatus() {
+        return shipmentStatus;
+    }
+
+    public void setShipmentStatus(ShipmentStatus shipmentStatus) {
+        this.shipmentStatus = shipmentStatus;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "Tracking{" +
+                "trackingId=" + trackingId +
+                ", shipmentStatus=" + shipmentStatus +
+                ", order=" + order +
+                ", station=" + station +
+                ", vehicle=" + vehicle +
+                '}';
+    }
+}
